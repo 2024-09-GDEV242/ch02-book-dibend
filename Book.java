83f8ccedfe606dcebda3507565a5a9f9f5c00a1b
NJWeb.Solutions <G00299250@stu.raritanval.edu>
2024-09-10 20:36:52 -0400

    update constructor to take numPages arg and store

--- Book.java
+++ Book.java
@@ -17,10 +17,11 @@ class Book
      * Set the author and title fields when this object
      * is constructed.
      */
-    public Book(String bookAuthor, String bookTitle)
+    public Book(String bookAuthor, String bookTitle, int numPages)
     {
         author = bookAuthor;
         title = bookTitle;
+        pages = numPages;
     }
 
     public String getAuthor() 
