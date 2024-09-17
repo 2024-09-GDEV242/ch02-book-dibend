fb53196442611ad017d9df237b200f4afd545d77
NJWeb.Solutions <david@njweb.solutions>
2024-09-17 13:56:25 -0400

    add comments

--- Book.java
+++ Book.java
@@ -3,8 +3,8 @@
  * This might form part of a larger application such
  * as a library system, for instance.
  *
- * @author (Insert your name here.)
- * @version (Insert today's date here.)
+ * @author David DiBenedetto
+ * @version 09-17-2024
  */
 class Book
 {
@@ -15,8 +15,12 @@ class Book
     private String refNumber;
 
     /**
-     * Set the author and title fields when this object
-     * is constructed.
+     * Constructor to initialize the author, title, and pages fields.
+     * The reference number is initialized to an empty string.
+     *
+     * @param bookAuthor The author of the book.
+     * @param bookTitle The title of the book.
+     * @param numPages The number of pages in the book.
      */
     public Book(String bookAuthor, String bookTitle, int numPages)
     {
@@ -26,31 +30,54 @@ class Book
         refNumber = "";
     }
 
+    /**
+     * Get the author of the book.
+     * @return The author of the book.
+     */
     public String getAuthor() 
     {
         return author;
     }
-    
+
+    /**
+     * Get the title of the book.
+     * @return The title of the book.
+     */
     public String getTitle()
     {
         return title;
     }
-    
+
+    /**
+     * Print the author of the book to the console.
+     */
     public void printAuthor()
     {
         System.out.println(author);
     }
-    
+
+    /**
+     * Print the title of the book to the console.
+     */
     public void printTitle()
     {
         System.out.println(title);
     }
-    
+
+    /**
+     * Get the number of pages in the book.
+     * @return The number of pages.
+     */
     public int getPages()
     {
         return pages;
     }
-    
+
+    /**
+     * Exercise 2.87: Print details of the book, including title, author, and pages.
+     * Additionally, print the reference number. If the reference number is not set,
+     * print "ZZZ" instead.
+     */
     public void printDetails()
     {
         System.out.println("****************************");
@@ -66,7 +93,13 @@ class Book
         System.out.println("Reference Number: " + ref);
         System.out.println("****************************");
     }
-    
+
+    /**
+     * Exercise 2.88: Set the reference number for the book if it meets the required length.
+     * Reference number must be at least 3 characters long.
+     *
+     * @param ref The reference number to be set.
+     */
     public void setRefNumber(String ref)
     {
         if (ref.length() < 3) {
@@ -77,7 +110,11 @@ class Book
             refNumber = ref;
         }
     }
-    
+
+    /**
+     * Get the reference number of the book.
+     * @return The reference number of the book.
+     */
     public String getRefNumber()
     {
         return refNumber;
